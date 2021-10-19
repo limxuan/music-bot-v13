d0d59b47dfa3acbd68715092454caac037cb01d7
Lim Xuan <olimxuan2005@gmail.com>
2021-10-19 09:16:30 +0800

    fix: file names

--- SlashCommands/music/pause.js
+++ SlashCommands/music/pause.js
@@ -1,43 +1,13 @@
-const { QueryType } = require("discord-player");
 const player = require("../../client/player");
 
 module.exports = {
-    name: "play",
-    description: "play a song",
-    options: [
-        {
-            name: "songtitle",
-            description: "title of the song",
-            type: "STRING",
-            required: true,
-        },
-    ],
+    name: "pause",
+    description: "pause the current song",
     run: async (client, interaction) => {
-        const songTitle = interaction.options.getString("songtitle");
+        const queue = player.getQueue(interaction.guildId);
 
-        if (!interaction.member.voice.channel)
-            return interaction.followUp({
-                content: "Please join a voice channel first!",
-            });
+        queue.setPaused(true);
 
-        const searchResult = await player.search(songTitle, {
-            requestedBy: interaction.user,
-            searchEngine: QueryType.AUTO,
-        });
-
-        const queue = await player.createQueue(interaction.guild, {
-            metadata: interaction.channel,
-        });
-
-        if (!queue.connection)
-            await queue.connect(interaction.member.voice.channel);
-
-        interaction.followUp({ content: `Playing ${songTitle}` });
-
-        searchResult.playlist
-            ? queue.addTracks(searchResult.tracks)
-            : queue.addTrack(searchResult.tracks[0]);
-
-        if (!queue.playing) await queue.play();
+        return interaction.followUp({ content: "Paused the current track!" });
     },
 };
--- SlashCommands/music/play.js
+++ SlashCommands/music/play.js
@@ -1,13 +1,43 @@
+const { QueryType } = require("discord-player");
 const player = require("../../client/player");
 
 module.exports = {
-    name: "pause",
-    description: "pause the current song",
+    name: "play",
+    description: "play a song",
+    options: [
+        {
+            name: "songtitle",
+            description: "title of the song",
+            type: "STRING",
+            required: true,
+        },
+    ],
     run: async (client, interaction) => {
-        const queue = player.getQueue(interaction.guildId);
+        const songTitle = interaction.options.getString("songtitle");
 
-        queue.setPaused(true);
+        if (!interaction.member.voice.channel)
+            return interaction.followUp({
+                content: "Please join a voice channel first!",
+            });
 
-        return interaction.followUp({ content: "Paused the current track!" });
+        const searchResult = await player.search(songTitle, {
+            requestedBy: interaction.user,
+            searchEngine: QueryType.AUTO,
+        });
+
+        const queue = await player.createQueue(interaction.guild, {
+            metadata: interaction.channel,
+        });
+
+        if (!queue.connection)
+            await queue.connect(interaction.member.voice.channel);
+
+        interaction.followUp({ content: `Playing ${songTitle}` });
+
+        searchResult.playlist
+            ? queue.addTracks(searchResult.tracks)
+            : queue.addTrack(searchResult.tracks[0]);
+
+        if (!queue.playing) await queue.play();
     },
 };

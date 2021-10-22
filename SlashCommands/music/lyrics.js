6ff3b52d68bded016941391c155202f599ee0781
reconlx <olimxuan2005@gmail.com>
2021-10-22 10:05:14 +0800

    feat(lyrics): notifies if lyrics is not found

--- SlashCommands/music/lyrics.js
+++ SlashCommands/music/lyrics.js
@@ -2,13 +2,18 @@ const player = require("../../client/player");
 const axios = require("axios");
 const { MessageEmbed } = require("discord.js");
 
-const getLyrics = async (title) => {
-    const url = new URL("https://some-random-api.ml/lyrics");
-    url.searchParams.append("title", title);
+const getLyrics = (title) =>
+    new Promise(async (ful, rej) => {
+        const url = new URL("https://some-random-api.ml/lyrics");
+        url.searchParams.append("title", title);
 
-    const { data } = await axios.get(url.href);
-    return data;
-};
+        try {
+            const { data } = await axios.get(url.href);
+            ful(data);
+        } catch (error) {
+            rej(error);
+        }
+    });
 
 const substring = (length, value) => {
     const replaced = value.replace(/\n/g, "--");
@@ -20,18 +25,24 @@ const substring = (length, value) => {
     return lines;
 };
 
-const createEmbeds = async (title) => {
-    const data = await getLyrics(title);
+const createResponse = async (title) => {
+    try {
+        const data = await getLyrics(title);
 
-    return substring(4096, data.lyrics).map((value, index) => {
-        const isFirst = index === 0;
+        const embeds = substring(4096, data.lyrics).map((value, index) => {
+            const isFirst = index === 0;
 
-        return new MessageEmbed({
-            title: isFirst ? `${data.title} - ${data.author}` : null,
-            thumbnail: isFirst ? { url: data.thumbnail.genius } : null,
-            description: value,
+            return new MessageEmbed({
+                title: isFirst ? `${data.title} - ${data.author}` : null,
+                thumbnail: isFirst ? { url: data.thumbnail.genius } : null,
+                description: value
+            });
         });
-    });
+
+        return { embeds };
+    } catch (error) {
+        return "I am not able to find lyrics for this song :(";
+    }
 };
 
 module.exports = {
@@ -42,15 +53,18 @@ module.exports = {
             name: "title",
             description: "specific song for lyrics",
             type: "STRING",
-            required: false,
-        },
+            required: false
+        }
     ],
     run: async (client, interaction) => {
         const title = interaction.options.getString("title");
         const sendLyrics = (songTitle) => {
-            return createEmbeds(songTitle)
-                .then((embeds) => interaction.followUp({ embeds }))
-                .catch(console.log);
+            return createResponse(songTitle)
+                .then((res) => {
+                    console.log({ res });
+                    interaction.followUp(res);
+                })
+                .catch((err) => console.log({ err }));
         };
 
         if (title) return sendLyrics(title);
@@ -58,9 +72,9 @@ module.exports = {
         const queue = player.getQueue(interaction.guildId);
         if (!queue?.playing)
             return interaction.followUp({
-                content: "No music is currently being played",
+                content: "No music is currently being played"
             });
 
         return sendLyrics(queue.current.title);
-    },
+    }
 };
